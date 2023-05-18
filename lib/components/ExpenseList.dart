c0741a1e19b2f501a6b05005cb282827c62a9eff
Arkar <69750819+Arkar20@users.noreply.github.com>
2023-05-18 18:44:26 +0630

    expense card

--- lib/components/ExpenseList.dart
+++ lib/components/ExpenseList.dart
@@ -11,8 +11,56 @@ class ExpenseList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: expenses.length, itemBuilder:(context, index) {
-        return Text(expenses[index].title);
-    }, );
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) {
+        return ExpenseCard(
+            title: expenses[index].title,
+            amount:'\$ ${expenses[index].amount.toStringAsFixed(2)}',
+            date: expenses[index].date.toIso8601String()
+            );
+      },
+    );
+  }
+}
+
+class ExpenseCard extends StatelessWidget {
+  const ExpenseCard({
+    super.key,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+
+ final String title;
+ final String amount;
+ final String date;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(amount),
+                  Row(
+                    children: [
+                      const Icon(Icons.abc_outlined),
+                     const SizedBox(width: 10,),
+                      Text(date)
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
+    );
   }
 }
